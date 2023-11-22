717cc2419744767ebf8d6de2c3ed5d139f5ab5bf
PhillipWong0627 <85791085+PhillipWong0627@users.noreply.github.com>
2023-11-22 18:01:00 +0800

    Profile Picture Page & Bar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,295 +1,41 @@
-<script>
-export default {
-  data() {
-    return {
-      homePlayer: [
-        //FIRST, CAPTAIN, SHIRT NUMBER, POSITION, PLAYERNAME
-        { id: 1, playerName: 'Player 1', image: 'defaultProfile', position: 'D', shirtNo: 2, captain: 1, first: 1 },
-        { id: 2, playerName: 'Player 2', image: 'defaultProfile', position: 'G', shirtNo: 2, captain: 1, first: 1 },
-        { id: 3, playerName: 'Player 3', image: 'defaultProfile', position: 'D', shirtNo: 20, captain: 1, first: 1 },
-        { id: 4, playerName: 'Player 4', image: 'defaultProfile', position: 'D', shirtNo: 2, captain: 1, first: 1 },
-        { id: 5, playerName: 'Player 5', image: 'defaultProfile', position: 'M', shirtNo: 2, captain: 1, first: 1 },
-        { id: 6, playerName: 'Player 6', image: 'defaultProfile', position: 'M', shirtNo: 12, captain: 1, first: 1 },
-        { id: 7, playerName: 'Player 7', image: 'defaultProfile', position: 'M', shirtNo: 2, captain: 1, first: 1 },
-        { id: 8, playerName: 'Player 8', image: 'defaultProfile', position: 'M', shirtNo: 2, captain: 1, first: 1 },
-        { id: 9, playerName: 'Player 9', image: 'defaultProfile', position: 'M', shirtNo: 2, captain: 1, first: 1 },
-        { id: 10, playerName: 'Player 10', image: 'defaultProfile', position: 'F', shirtNo: 2, captain: 1, first: 1 },
-        { id: 11, playerName: 'Player 11', image: 'defaultProfile', position: 'F', shirtNo: 2, captain: 1, first: 1 },
-
-      ],
-      awayPlayer: [
-        { id: 1, playerName: 'Player 11', image: 'defaultProfile', position: 'S', shirtNo: 2, captain: 1, first: 1 },
-
-        { id: 2, playerName: 'Player 13', image: 'defaultProfile', position:'F', shirtNo: 2, captain: 1, first: 1 },
-        { id: 3, playerName: 'Player 14', image: 'defaultProfile', position:'F', shirtNo: 2, captain: 1, first: 1 },
-        { id: 4, playerName: 'Player 15', image: 'defaultProfile', position:'F', shirtNo: 2, captain: 1, first: 1 },
-
-        { id: 5, playerName: 'Player 16', image: 'defaultProfile', position:'M', shirtNo: 2, captain: 1, first: 1 },
-        { id: 6, playerName: 'Player 17', image: 'defaultProfile', position:'M', shirtNo: 2, captain: 1, first: 1 },
-
-        { id: 7, playerName: 'Player 16', image: 'defaultProfile', position:'D', shirtNo: 2, captain: 1, first: 1 },
-        { id: 8, playerName: 'Player 17', image: 'defaultProfile', position:'D', shirtNo: 2, captain: 1, first: 1 },
-        { id: 9, playerName: 'Player 18', image: 'defaultProfile', position:'D', shirtNo: 2, captain: 1, first: 1 },
-        { id: 10, playerName: 'Player 19', image: 'defaultProfile', position:'D', shirtNo: 2, captain: 1, first: 1  },
-
-        { id: 11, playerName: 'Player', image: 'defaultProfile', position:  'G', shirtNo: 2, captain: 1, first: 1 },
-      ]
-    };
-  },
-  computed: {
-    filteredHomePlayers() {
-      return (position) => this.homePlayer.filter(player => player.position === position);
-    },
-    filteredAwayPlayers(){
-      return (position) => this.awayPlayer.filter(player => player.position === position);
-    },
-    hasHomeSPlayers() {
-      return this.homePlayer.some(player => player.position === 'S');
-    },
-    hasAwaySPlayers() {
-      return this.awayPlayer.some(player => player.position === 'S');
-    },
-  }
-};
-</script>
-
 <template>
-  <div class="flex justify-start">
-    <div class="overflow-hidden">
-      <div class="soccer-field flex">
-
-        <!-- Home Players -->
-        <div class="w-1/2 h-full flex justify-between">
-          <!-- IF G PASS HERE -->
-          <div class="w-full flex flex-col justify-around">
-            <div v-for="player in filteredHomePlayers('G')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="homeKeeperCircle flex justify-center items-center">
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-          </div>
-          <!-- IF D PASS HERE -->
-          <div class="w-full flex flex-col justify-around">
-            <div v-for="player in filteredHomePlayers('D')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="homeCircle flex justify-center items-center">
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-          </div>
-
-
-          <!-- IF M PASS HERE -->
-          <div class="w-full flex flex-col justify-around">
-            <div v-for="player in filteredHomePlayers('M')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="homeCircle flex justify-center items-center">
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-          </div>
-
-          <!-- IF F PASS HERE -->
-          <div class="w-full flex flex-col justify-around">
-            <div v-for="player in filteredHomePlayers('F')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="homeCircle flex justify-center items-center">
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-
-            <div class="absolute bottom-1" style="left: 285px; ">
-              <div class="flex pb-2">
-                <span class="pl-2.5 pr-1 pt-1.5 text-xs font-normal text-white">3-5-2</span>
-                <img class="" style="width: 24px; height: 24px;" src="../assets/tournament/moneyBadge.png" />
-              </div>
-            </div>
-          </div>
-
-          <!-- 英文版有S 的球员 -->
-          <div class="w-full flex flex-col justify-around" v-if="hasHomeSPlayers">
-            <div v-for="player in filteredHomePlayers('S')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="homeCircle flex justify-center items-center">
-                  <!-- <img class="" src="../assets/tournament/playerIcon.png" /> -->
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- AWAY TEAM -->
-        <div class="w-1/2 h-full flex justify-between">
-          <!-- 英文版有S 的球员 -->
-          <div class="w-full flex flex-col justify-around" v-if="hasAwaySPlayers">
-            <div v-for="player in filteredAwayPlayers('S')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="awayCircle flex justify-center items-center">
-                  <!-- <img class="" src="../assets/tournament/playerIcon.png" /> -->
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full flex flex-col justify-around">
-            <div v-for="player in filteredAwayPlayers('F')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="awayCircle flex justify-center items-center">
-                  <!-- <img class="" src="../assets/tournament/playerIcon.png" /> -->
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-
-            <div class="absolute bottom-1" style="left: 370px; ">
-              <div class="flex pb-1.5">
-                <img class="" style="width: 24px; height: 24px;" src="../assets/tournament/badge_.png" />
-                <span class="pl-1.5 pr-1 pt-1 text-xs font-normal text-white">4-2-3-1</span>
-              </div>
-            </div>
-
-          </div>
-          <div class="w-full flex flex-col justify-around" >
-            <div v-for="player in filteredAwayPlayers('M')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="awayCircle flex justify-center items-center">
-                  <!-- <img class="" src="../assets/tournament/playerIcon.png" /> -->
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full flex flex-col justify-around" >
-            <div v-for="player in filteredAwayPlayers('D')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="awayCircle flex justify-center items-center">
-                  <!-- <img class="" src="../assets/tournament/playerIcon.png" /> -->
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full flex flex-col justify-around" >
-            <div v-for="player in filteredAwayPlayers('G')" :key="player.id" class="">
-              <div class="flex flex-col items-center">
-                <div class="awayKeeperCircle flex justify-center items-center">
-                  <!-- <img class="" src="../assets/tournament/playerIcon.png" /> -->
-                  <div>
-                    <p class="text-white text-sm font-normal"> {{ player.shirtNo }}</p>
-                  </div>
-                </div>
-                <p>球员名字</p>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-
+  <div class="team-stats">
+    <div class="team">
+      <h2>Team A</h2>
+      <div class="stat-bar" :style="{ width: controlRateA + '%' }"></div>
+      <p>Control Rate: {{ controlRateA }}%</p>
+      <!-- Add more statistics as needed -->
     </div>
 
+    <div class="team">
+      <h2>Team B</h2>
+      <div class="stat-bar" :style="{ width: controlRateB + '%' }"></div>
+      <p>Control Rate: {{ controlRateB }}%</p>
+      <!-- Add more statistics as needed -->
+    </div>
   </div>
 </template>
 
+<script setup>
+const controlRateA = 60; // Replace with your actual data
+const controlRateB = 40; // Replace with your actual data
+// Add more data properties for other statistics
+</script>
 
 <style scoped>
-.soccer-field {
-  position: relative;
-  width: 728px;
-  height: 343px;
-  background-size: contain;
-  background: url('../assets/tournament/football_court.png') center/cover;
+.team-stats {
+  display: flex;
+  justify-content: space-around;
+  margin: 20px;
 }
 
-.player {
-  position: absolute;
-  transform: translate(-50%, -50%);
+.team {
   text-align: center;
 }
 
-.avatar {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
-
-.player-name {
-  display: block;
-  margin-top: 5px;
-  font-size: 12px;
-  font-weight: 400;
-}
-
-.homeCircle {
-  width: 28px;
-  height: 28px;
-  background-color: #1168B9;
-  /* Set the background color */
-  border-radius: 50%;
-  /* Make it a circle by setting border-radius to 50% */
-  border: 2px solid black;
-}
-
-.awayCircle {
-  width: 28px;
-  height: 28px;
-  background-color: #D043D3;
-  /* Set the background color */
-  border-radius: 50%;
-  /* Make it a circle by setting border-radius to 50% */
-  border: 2px solid black;
-}
-
-.homeKeeperCircle {
-  width: 28px;
-  height: 28px;
-  background-color: #FC6A28;
-  /* Set the background color */
-  border-radius: 50%;
-  /* Make it a circle by setting border-radius to 50% */
-  border: 2px solid black;
-}
-
-.awayKeeperCircle {
-  width: 28px;
-  height: 28px;
-  background-color: #FFC42B;
-  /* Set the background color */
-  border-radius: 50%;
-  /* Make it a circle by setting border-radius to 50% */
-  border: 2px solid black;
+.stat-bar {
+  height: 20px;
+  background-color: #3498db; /* Set the background color of the bar */
+  margin-bottom: 5px;
 }
 </style>
--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,120 +1,76 @@
 <template>
-  <div class="login-form">
-    <div>
-      <h2 class="text-xl font-bold">登入使用</h2>
-      <div class="form-group flex">
-        <!-- Country Code Dropdown -->
-        <select v-model="userData.countryCode" class="mr-2">
-          <option value="+86">+86</option>
-          <option value="+60">+60</option>
-          <!-- Add more country codes as needed -->
-        </select>
-        <input placeholder="请输入手机号码"  type="text" id="phone" v-model="userData.phone" />
-      </div>
+  
+  <div class="profileForm flex flex-col items-center">
+    <!-- Above -->
+    <div class="flex flex-col justify-around items-center " style="height: 278px; width: 343px;">
+      <div>
+        <div class="">
+          <form class="flex flex-col items-center" action="">
+            <div class="relative">
+              <img src="@/assets/ProfilePicture.png" style="width: 114px; height: 114px;" />
+              <!-- Add upload button here -->
+              <label for="upload" class="absolute bottom-0 right-0 cursor-pointer">
+                <img src="@/assets/upload-icon.png" style="width: 24px; height: 24px;" />
+                <input type="file" id="upload" style="display: none;" @change="handleImageUpload" />
+              </label>
+            </div>
+          </form>
 
-      <div class="form-group">
-        <div class="password-container">
-          <input placeholder="请输入密码" type="password" id="password" v-model="userData.password" />
-          <button class="button mr-1.5">
-            <img class="" src="../assets/hide.png" />
-          </button>
+        </div>
+        <div class="flex flex-col items-center pt-5">
+          <p class="text-lg font-semibold ">NIAMAMAX</p>
+          <p class="text-sm font-medium">01113393481</p>
         </div>
       </div>
-      <router-link to="#" class="flex justify-end"><p class="text-black font-bold">忘记密码 ?</p></router-link>
-    </div>
-    <div class="pt-10">
-      <ButtonCom @click="login" class="w-screen">登入</ButtonCom>
-      <!-- <button @click="register">登入</button> -->
-      <div class="flex justify-center">
-        <p>还没有账号？ <router-link to = "/register" class="text-green-500">现在注册</router-link></p>
+
+      <div class="flex justify-around pt-7" style="width: 100%;">
+        <div>
+          <ButtonPress class="w-full bg-white" style="width: 105px; height: 60px; border-radius: 8px;">
+            <p class="text-base font-normal" style="color: #333333;">系统消息</p>
+          </ButtonPress>
+
+        </div>
+        <div>
+          <ButtonPress class="w-full bg-white" style="width: 105px; height: 60px;">
+            <p class="text-base font-normal" style="color: #333333;">联系我们</p>
+          </ButtonPress>
+
+        </div>
+        <div>
+          <ButtonPress class="w-full bg-white" style="width: 105px; height: 60px;">
+            <p class="text-base font-normal" style="color: #333333;">系统设置</p>
+          </ButtonPress>
+
+        </div>
       </div>
     </div>
-    
+    <ButtonPress class="w-full my-14" style="width: 331px; height: 56px;">
+      <p class="text-base font-semibold">退出登录</p>
+    </ButtonPress>
+
+
   </div>
 </template>
 
 <script setup>
-import ButtonCom from './ButtonPress.vue';
-import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import ButtonPress from '@/components/ButtonPress.vue'
 
-const userData = ref({
-  countryCode: '+86', // Default country code
-  phone: '',
-  password: ''
-});
-
-
-const login = () => {
-  // if (userData.value.password !== confirmPassword.value) {
-  //   alert('Passwords do not match');
-  // } else {
-  //   // Perform login logic here, e.g., send user data to the server
-  //   // Example: axios.post("/api/login", userData.value).then(response => { ... });
-  // }
-};
 </script>
 
 
 <style scoped>
-.login-form {
-  max-width: 400px;
+.profileForm {
+  max-width: 375px;
+  max-height: 514px;
   margin: 0 auto;
   padding: 20px;
+  padding-top: 50px;
   border: 1px solid #ccc;
   border-radius: 5px;
+  background-color: #F4F9F4;
+
 }
 
-select{
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-.password-container {
-display: flex;
-align-items: center;
-/* justify-content: space-between; */
-width: 100%;
-position: relative;
-}
-#password {
-  flex: 1;
-  padding-right: 30px; /* Space for the toggle button */
-}
-.button {
-  position: absolute;
-  right: 5px;
-  background: none;
-  border: none;
-  cursor: pointer;
-}
-.form-group {
-  margin-bottom: 15px;
-}
 
-label {
-  display: block;
-  font-weight: bold;
-}
 
-input {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  font-size: 16px;
-}
-
-  /* button {
-    width: 100%;
-    padding: 10px;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 16px;
-  } */
-
-  /* button:hover {
-    background-color: #0056b3;
-  } */
 </style>
